6e1fe78453045f4f2a680644fdecaa2ca869641b
Felix Chapman <aelred717@gmail.com>
2017-06-27 15:37:16 +0100

    Resolve code review issues

--- env.sh
+++ env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ -d "/Library/Caches" ] ; then
-    CACHE_DIR="/Library/Caches/grakn-spec"
-elif [ -d "/usr/local/share" ] ; then
-    CACHE_DIR="/usr/local/share/grakn-spec"
+set -euo pipefail
+
+if [ -d "$HOME/Library/Caches" ] ; then
+    CACHE_DIR="$HOME/Library/Caches/SuperApp"
+elif [ -d "$HOME/.cache" ] ; then
+    CACHE_DIR="$HOME/.cache/grakn-spec"
 else
     >&2 echo "Could not find cache directory"
     exit 1
@@ -16,7 +18,7 @@ GRAKN_RELEASE="grakn-dist-${VERSION}"
 
 GRAKN_TAR="${GRAKN_RELEASE}.tar.gz"
 
-TEMP_DIR_PTR="$CACHE_DIR/temp-dir"
+GRAKN_DIR="${CACHE_DIR}/grakn"
 
 case $1 in
     start)
@@ -27,7 +29,7 @@ case $1 in
         set +e
         nc -z localhost 4567
         PORT_IN_USE=$?
-        set -e
+        set -euo pipefail
 
         if [ $PORT_IN_USE -eq 0 ]; then
             >&2 echo "Port 4567 is in use. Maybe a Grakn server is already running?"
@@ -40,14 +42,9 @@ case $1 in
             wget -O "$DOWNLOAD_PATH" "$DOWNLOAD_URL"
         fi
 
-        TEMP_DIR=`mktemp -d`
-        echo "$TEMP_DIR" > "$TEMP_DIR_PTR"
+        tar -xvf "${DOWNLOAD_PATH}" -C "${CACHE_DIR}"
 
-        tar -xvf "${DOWNLOAD_PATH}" -C "${TEMP_DIR}"
-
-        GRAKN_DIR="${TEMP_DIR}/grakn"
-
-        mv "${TEMP_DIR}/${GRAKN_RELEASE}" "$GRAKN_DIR"
+        mv "${CACHE_DIR}/${GRAKN_RELEASE}" "$GRAKN_DIR"
 
         "${GRAKN_DIR}/bin/grakn.sh" start
         sleep 5  # TODO: remove this when `grakn.sh start` blocks
@@ -57,13 +54,8 @@ case $1 in
         "${GRAKN_DIR}/bin/graql.sh" -f "$GRAQL_FILE_OF_TYPES_AND_INSTANCES"
         ;;
     stop)
-        TEMP_DIR=$(<"${TEMP_DIR_PTR}")
-
-        GRAKN_DIR="${TEMP_DIR}/grakn"
-
         "${GRAKN_DIR}/bin/grakn.sh" stop
-
-        rm -rf "$TEMP_DIR"
+        rm -rf "$GRAKN_DIR"
         ;;
     *)
         >&2 echo 'Valid commands are `start` and `stop`'
